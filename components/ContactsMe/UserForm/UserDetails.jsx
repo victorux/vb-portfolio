0b4250de809aab1adbfd2a266a1713ea2654d173
victorb <hi.victorb@gmail.com>
2024-05-31 11:31:33 +0100

    email send test 2

--- components/ContactsMe/UserForm/UserDetails.jsx
+++ components/ContactsMe/UserForm/UserDetails.jsx
@@ -1,80 +1,96 @@
-import React, { Component } from 'react'
-import styles from '../../../styles/contacts.module.scss'
-import Btn from '../Chat/Btn';
+import React, { Component } from "react";
+import styles from "../../../styles/contacts.module.scss";
+import Btn from "../Chat/Btn";
 
 const UserDetails = (props) => {
   const formik = props.formik;
 
   const prevStep = () => {
-    formik.setFieldValue('step', 1)
-  }
+    formik.setFieldValue("step", 1);
+  };
 
   const nextStep = () => {
-    formik.setFieldValue('step', 3)
+    formik.setFieldValue("step", 3);
+  };
+
+  // Send email
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+    const formData = formik.values;
+    await fetch("https://victorbotan.com/api/mail", {
+      method: "post",
+      body: JSON.stringify(formData),
+    }).then(() => nextStep());
   }
 
-    // Send email
-    async function handleOnSubmit(e){
-      e.preventDefault();
-      const formData = formik.values;
-      fetch('https://victorbotan.com/api/mail', {
-        method: 'post',
-        body: JSON.stringify(formData)
-      }).then(() =>  nextStep())
-    }
-
-    return (
-      <div className={styles.animateChat}>
-        <span className={styles.titleUserDetails}>🦹‍♀️ Tell a bit about yourself</span>
-        <div className={styles.userInputs}>
-          <input
-            placeholder='Your full name or company name'
-            id='fullName'
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            defaultValue={formik.values.fullName}
-            className={formik.errors.fullName && formik.touched.fullName ? [styles.user_input, styles.inputError].join(' ') : styles.user_input}
-          />
-          {
-            formik.errors.fullName && formik.touched.fullName 
-            ? <span className={styles.error}>{formik.errors.fullName}</span> 
-            : null
+  return (
+    <div className={styles.animateChat}>
+      <span className={styles.titleUserDetails}>
+        🦹‍♀️ Tell a bit about yourself
+      </span>
+      <div className={styles.userInputs}>
+        <input
+          placeholder="Your full name or company name"
+          id="fullName"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          defaultValue={formik.values.fullName}
+          className={
+            formik.errors.fullName && formik.touched.fullName
+              ? [styles.user_input, styles.inputError].join(" ")
+              : styles.user_input
           }
-          <input 
-            placeholder='Your Email'
-            id='email'
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            defaultValue={formik.values.email}
-            className={formik.errors.email && formik.touched.email ? [styles.user_input, styles.inputError].join(' ') : styles.user_input}
-          />
-          {
-            formik.errors.email && formik.touched.email 
-            ? <span className={styles.error}>{formik.errors.email}</span> 
-            : null
+        />
+        {formik.errors.fullName && formik.touched.fullName ? (
+          <span className={styles.error}>{formik.errors.fullName}</span>
+        ) : null}
+        <input
+          placeholder="Your Email"
+          id="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          defaultValue={formik.values.email}
+          className={
+            formik.errors.email && formik.touched.email
+              ? [styles.user_input, styles.inputError].join(" ")
+              : styles.user_input
           }
-          <input 
-            placeholder='Contact Number (optional)' 
-            id='phoneNumber'
-            type="number"
-            maxLength="14"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            defaultValue={formik.values.phoneNumber}
-            className={formik.errors.phoneNumber && formik.touched.phoneNumber ? [styles.user_input, styles.noSpin, styles.inputError].join(' ') : [styles.user_input, styles.noSpin].join(' ')}
-          />
-          {
-            formik.errors.phoneNumber && formik.touched.phoneNumber 
-            ? <span className={styles.error}>{formik.errors.phoneNumber}</span> 
-            : null
+        />
+        {formik.errors.email && formik.touched.email ? (
+          <span className={styles.error}>{formik.errors.email}</span>
+        ) : null}
+        <input
+          placeholder="Contact Number (optional)"
+          id="phoneNumber"
+          type="number"
+          maxLength="14"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          defaultValue={formik.values.phoneNumber}
+          className={
+            formik.errors.phoneNumber && formik.touched.phoneNumber
+              ? [styles.user_input, styles.noSpin, styles.inputError].join(" ")
+              : [styles.user_input, styles.noSpin].join(" ")
           }
-        </div>
-        <div className={styles.navigation}>
-          <Btn title="Back" click={prevStep} bg='gray' text='white'  />
-          <button disabled={formik.errors.fullName || formik.errors.email? true : false} onClick={handleOnSubmit} className={styles.button}>→</button>
-        </div>
+        />
+        {formik.errors.phoneNumber && formik.touched.phoneNumber ? (
+          <span className={styles.error}>{formik.errors.phoneNumber}</span>
+        ) : null}
       </div>
-    )
-}
+      <div className={styles.navigation}>
+        <Btn title="Back" click={prevStep} bg="gray" text="white" />
+        <button
+          disabled={
+            formik.errors.fullName || formik.errors.email ? true : false
+          }
+          onClick={handleOnSubmit}
+          className={styles.button}
+        >
+          →
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default UserDetails
+export default UserDetails;
--- pages/api/mail.js
+++ pages/api/mail.js
@@ -2,43 +2,25 @@ const mail = require("@sendgrid/mail");
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: "hi.victorb@gmail.com", // Change to your recipient
-  from: "hi.victorb@gmail.com", // Change to your verified sender
-  subject: "Sending with SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-};
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log("Email sent");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+export default function handler(req, res) {
+  const body = JSON.parse(req.body);
 
-// export default function handler(req, res) {
-//   const body = JSON.parse(req.body);
+  const message = `
+    Message: ${body.message}\r\n
+    Email: ${body.email}\r\n
+    FullName: ${body.fullName}\r\n
+    FullName: ${body.phoneNumber}
+  `;
 
-//   const message = `
-//     Message: ${body.message}\r\n
-//     Email: ${body.email}\r\n
-//     FullName: ${body.fullName}\r\n
-//     FullName: ${body.phoneNumber}
-//   `
+  const data = {
+    to: "hi.victorb@gmail.com",
+    from: "hi.victorb@gmail.com",
+    subject: "New Project Proposal",
+    text: message,
+    html: message.replace(/\r\n/g, "<br>"),
+  };
 
-//   const data = {
-//     to: 'hi.victorb@gmail.com',
-//     from: 'hi.victorb@gmail.com',
-//     subject: 'New Project Proposal',
-//     text: message,
-//     html: message.replace(/\r\n/g, '<br>')
-//   }
+  mail.send(data);
 
-//   mail.send(data)
-
-//   res.status(200).json({ status: 'Ok' })
-// }
+  res.status(200).json({ status: "Ok" });
+}
